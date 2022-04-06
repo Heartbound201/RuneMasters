f32bc535292aa3735af6b892d7598abea61ba68d
Andrea Cancellieri <andrea.cancellieri95@gmail.com>
2022-04-07 00:52:46 +0200

    Modified party bars implementation to be more flexible with value changes

--- Assets/Scripts/UI/PartyInfoMenuController.cs
+++ Assets/Scripts/UI/PartyInfoMenuController.cs
@@ -12,9 +12,13 @@ public class PartyInfoMenuController : MonoBehaviour
     public TextMeshProUGUI partyManaMaxText;
     public TextMeshProUGUI partyManaReserveText;
     public TextMeshProUGUI partyManaReserveMaxText;
+
     public Image HealthBar;
+    public GameObject HealthBarContainer;
     public Image ManaBar;
+    public GameObject ManaBarContainer;
     public Image ReserveBar;
+    public GameObject ReserveBarContainer;
     public Button endTurnBtn;
     
     public void UpdatePartyInfo(Party party)
@@ -26,9 +30,13 @@ public class PartyInfoMenuController : MonoBehaviour
         partyManaReserveText.text = party.ManaReserve.ToString();
         partyManaReserveMaxText.text = party.ManaReserveMax.ToString();
 
-        HealthBar.GetComponent<RectTransform>().sizeDelta = new Vector2(5 * party.Health, HealthBar.GetComponent<RectTransform>().rect.height);
-        ManaBar.GetComponent<RectTransform>().sizeDelta = new Vector2(5 * party.Mana, ManaBar.GetComponent<RectTransform>().rect.height);
-        ReserveBar.GetComponent<RectTransform>().sizeDelta = new Vector2(5 * party.ManaReserve, ReserveBar.GetComponent<RectTransform>().rect.height);
+		HealthBar.GetComponent<RectTransform>().sizeDelta = new Vector2(5 * party.Health, HealthBar.GetComponent<RectTransform>().rect.height);
+		HealthBarContainer.GetComponent<RectTransform>().sizeDelta = new Vector2(5 * party.HealthMax, HealthBar.GetComponent<RectTransform>().rect.height);
+
+		ManaBar.GetComponent<RectTransform>().sizeDelta = new Vector2(5 * party.Mana, ManaBar.GetComponent<RectTransform>().rect.height);
+		ManaBarContainer.GetComponent<RectTransform>().sizeDelta = new Vector2(5 * party.ManaMax, ManaBar.GetComponent<RectTransform>().rect.height);
+        
+		ReserveBar.GetComponent<RectTransform>().sizeDelta = new Vector2(5 * party.ManaReserve, ReserveBar.GetComponent<RectTransform>().rect.height);
+		ReserveBarContainer.GetComponent<RectTransform>().sizeDelta = new Vector2(5 * party.ManaReserveMax, ReserveBar.GetComponent<RectTransform>().rect.height);
     }
-    
 }
